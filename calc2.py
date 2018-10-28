b0d70260d5d3c4367449d71ef21421b621a7658e
Joseph Marc Antoine Ridore <Marc>
2018-10-28 17:24:03 -0400

    added my github name

---
+++ calc2.py
@@ -0,0 +1,15 @@
+def Multiply(num1, num2):
+    answer = num1 * num2
+    print(answer)
+    
+def Divide(num1, num2):
+     answer2 = num1 / num2
+     print(answer2)   
+
+def Sum(num1, num2):
+     answer3 = num1 + num2
+     print(answer3)  
+Multiply(4,8)
+
+Divide(4,8)
+Sum(7,8)
